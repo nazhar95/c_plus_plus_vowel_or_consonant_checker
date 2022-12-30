79299bab28eb2ffdcdf88d7cc6e5b314ffdcee77
Naz Azhar <57789624+nazhar95@users.noreply.github.com>
2022-12-30 11:09:50 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+void check(){
+    char character;
+    bool isLowerCaseVowel, isUppercaseVowel;
+    
+    cout << "Enter a character: ";
+    cin >> character;
+    
+    //check if the character is lowercase vowel
+    isLowerCaseVowel = (character == 'a' || character =='e' || character =='i' || character == 'o' || character == 'u'); 
+    
+    //check if the character is uppercase vowel
+    isUppercaseVowel = (character == 'A' || character =='E' || character =='I' || character == 'O' || character == 'U'); 
+    
+    //If the character entered by user is not an alphabet
+    if(!isalpha(character)){
+        cout << "Error! You have entered a non-alphabetical character." << endl;
+    }
+    else if (isLowerCaseVowel || isUppercaseVowel){
+        cout << "\"" << character << "\"" << " is a vowel." << endl;
+    }
+    else{
+        cout <<  "\"" << character << "\""  << " is a consonant." << endl;
+    }
+}
+
+int main()
+{
+    char choice;
+    
+    do{
+        check();
+        cout << "Would you like to continue checking? \n";
+        cout << "Press Y if you would like to continue. \n";
+        cin >> choice;
+    }while(choice=='Y'||choice=='y');
+
+    return 0;
+}
